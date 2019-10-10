6ba47472fb27a08e333634d03d4accdd07660a98
Joaquim Ferrer <jvrbferrer@gmail.com>
2019-10-10 16:54:55 +0200

    Adds run

--- run.py
+++ run.py
@@ -9,12 +9,14 @@ def main():
     env = gym.envs.make("CartPole-v0")
 
     net = torch.nn.Sequential(
-            torch.nn.Linear(np.prod(env.observation_space.shape), env.action_space.n),
-        )
+        torch.nn.Linear(np.prod(env.observation_space.shape), 128),
+        torch.nn.ReLU(),
+        torch.nn.Linear(128, env.action_space.n),
+    )
 
     approximator = Approximator(net, alpha=1e-3, loss=torch.nn.MSELoss)
     train(approximator, env,
-          n_step=0,
+          n_step=2,
           n_episodes=int(1e2),
           gamma=0.8,
           semi_gradient=True,
