67eaab143dc4e218b63a6e85f7e60392142552ca
Maurice Frank <maurice.frank@posteo.de>
2019-10-10 12:43:44 +0200

    Added changeable loss

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ class AList(list):
 
 
 class Approximator(torch.nn.Module):
-    
-    def __init__(self, n_in, n_out, alpha, optimizer=torch.optim.Adam):
+
+    def __init__(self, n_in, n_out, alpha, optimizer=torch.optim.Adam, loss=torch.nn.SmoothL1Loss):
         super(Approximator, self).__init__()
         self.layer_1 = torch.nn.Linear(n_in, 128)
         self.layer_2 = torch.nn.Linear(128, n_out)
         self.optimizer = optimizer(self.parameters(), lr=alpha)
+        self.loss_function = loss
 
     def forward(self, x):
         return self.layer_2(torch.nn.functional.relu(self.layer_1(torch.FloatTensor(x))))
-    
+
     """
     G: descounted reward so far
     gamma: discount of the last_state_action
@@ -39,7 +40,8 @@ class Approximator(torch.nn.Module):
             torch.set_grad_enabled(False)
         G = G + (gamma * (self(last_state_action[0])[last_state_action[1]] if last_state_action != None else 0))
         torch.set_grad_enabled(True)
-        loss = (G - self(target_state_action[0])[target_state_action[1]])**2
+        target = self(target_state_action[0])[target_state_action[1]]
+        loss = self.loss_function(G, target)
         loss.backward()
         self.optimizer.step()
         return loss
@@ -79,7 +81,7 @@ def train(approximator: Approximator, env: gym.Env, n_step: int, n_episodes: int
                 G = np.sum(rewards[τ:t+1] * np.power(gamma, np.linspace(0, n_step-1, n_step)))
                 #print("Inside", t, len(actions))
                 action = max_actions[t] if q_learning else actions[t]
-                last_state_action = (states[t], action) if not done else None 
+                last_state_action = (states[t], action) if not done else None
                 loss = approximator.update_weights(G, gamma**n_step, last_state_action, (states[τ], actions[τ]), semi_gradient)
             if τ == T - 1:
                 durations[i_episode] = len(states)
@@ -87,7 +89,7 @@ def train(approximator: Approximator, env: gym.Env, n_step: int, n_episodes: int
                 print(returns[i_episode])
                 #print(loss)
                 break
-    
+
     return returns
 
 def main():
