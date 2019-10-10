2311f35515eb9b3cdb9b99e95364215f00ea6ac6
Maurice Frank <maurice.frank@posteo.de>
2019-10-10 16:14:40 +0200

    updt params

--- run.py
+++ run.py
@@ -10,11 +10,17 @@ def main():
     env = gym.envs.make(args.env)
 
     env = gym.envs.make("CartPole-v0")
-    hidden = 128
+    hidden1 = 128
+    hidden2 = 64
+    hidden3 = 32
     net = torch.nn.Sequential(
-            torch.nn.Linear(np.prod(env.observation_space.shape), hidden),
+            torch.nn.Linear(np.prod(env.observation_space.shape), hidden1),
             torch.nn.ReLU(),
-            torch.nn.Linear(hidden, env.action_space.n),
+            torch.nn.Linear(hidden1, hidden2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden2, hidden3),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden3, env.action_space.n),
         )
 
     approximator = Approximator(net, alpha=1e-3, loss=torch.nn.MSELoss)
@@ -22,7 +28,7 @@ def main():
           n_step=2,
           n_episodes=int(1e2),
           gamma=0.8,
-          semi_gradient=True,
+          semi_gradient=False,
           q_learning=True,
           n_memory=10000,
           batch_size=64,
