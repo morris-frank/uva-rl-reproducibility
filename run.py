67e8efdf780236aa6a8bb631d1cb1ab1ea53e5c6
Maurice Frank <maurice.frank@posteo.de>
2019-10-10 16:52:11 +0200

    updt params

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gym
-import torch
+from torch import nn
 import numpy as np
 import argparse
 
@@ -7,32 +7,31 @@ from src import train, Approximator
 
 
 def main():
-    env = gym.envs.make(args.env)
+    env = gym.envs.make("Assault-ram-v0")
 
-    env = gym.envs.make("CartPole-v0")
-    hidden1 = 128
-    hidden2 = 64
-    hidden3 = 32
-    net = torch.nn.Sequential(
-            torch.nn.Linear(np.prod(env.observation_space.shape), hidden1),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden1, hidden2),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden2, hidden3),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden3, env.action_space.n),
+    isize = np.prod(env.observation_space.shape)
+    print(isize)
+    osize = env.action_space.n
+    hidden = [128, 64, 32]
+
+    net = nn.Sequential(
+            nn.Linear(isize, hidden[0]),
+            nn.ReLU(),
+            nn.Linear(hidden[0], hidden[1]),
+            nn.ReLU(),
+            nn.Linear(hidden[1], osize),
         )
 
-    approximator = Approximator(net, alpha=1e-3, loss=torch.nn.MSELoss)
+    approximator = Approximator(net, alpha=1e-3, loss=nn.MSELoss)
     train(approximator, env,
           n_step=2,
           n_episodes=int(1e2),
           gamma=0.8,
-          semi_gradient=False,
+          semi_gradient=True,
           q_learning=True,
           n_memory=10000,
           batch_size=64,
-          render=False)
+          render=True)
 
 
 if __name__ == "__main__":
