0da88322089a19d668594ef9c416d61e6fd19ac1
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-10-10 18:58:58 +0200

    more argparse

--- run.py
+++ run.py
@@ -10,6 +10,9 @@ def main():
     parser = argparse.ArgumentParser(description='play RL games')
     parser.add_argument('--env', default='CartPole-v0', help='https://gym.openai.com/envs/')
     parser.add_argument('--seed', default=0, help='random seed')
+    parser.add_argument('--alpha', default=1e-3, help='learning rate')
+    parser.add_argument('--gamma', default=0.8, help='reward decay')
+    parser.add_argument('--n_episodes', default=100, help='number of episodes to play')
     args = parser.parse_args()
 
     env = gym.envs.make(args.env)
@@ -27,11 +30,11 @@ def main():
         torch.nn.Linear(128, env.action_space.n),
     )
 
-    approximator = Approximator(net, alpha=1e-3, loss=torch.nn.MSELoss)
+    approximator = Approximator(net, alpha=args.alpha, loss=torch.nn.MSELoss)
     train(approximator, env,
           n_step=2,
-          n_episodes=int(1e2),
-          gamma=0.8,
+          n_episodes=args.n_episodes,
+          gamma=args.gamma,
           semi_gradient=True,
           q_learning=True,
           n_memory=10000,
