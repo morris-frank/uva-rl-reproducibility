c12cca62f67512de1ac0b289759ab4ce39e5ee86
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-10-10 15:48:35 +0200

    rm duplicate

--- src/memory.py
+++ src/memory.py
@@ -26,11 +26,3 @@ class Memory(object):
         :return:
         """
         return random.sample(self._mem, n)
-
-def get_get_epsilon(it_at_min, min_epsilon):
-    def get_epsilon(it):
-        if it >= it_at_min:
-            return min_epsilon
-        else:
-            return -((1-min_epsilon)/it_at_min)*it + 1
-    return get_epsilon
