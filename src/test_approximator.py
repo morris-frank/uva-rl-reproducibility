d47e9047c056cbed890af17a9f5a76ce68ae6d9e
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-10-10 17:05:32 +0200

    test approximator

--- src/test_approximator.py
+++ src/test_approximator.py
@@ -3,14 +3,32 @@ import torch
 from .approximator import Approximator
 
 def test_approximator():
-    hidden = 3
+    I = 3
+    H = 2
+    O = 4
     net = torch.nn.Sequential(
-            torch.nn.Linear(3, hidden),
+            torch.nn.Linear(I, H),
             torch.nn.ReLU(),
-            torch.nn.Linear(hidden, 4),
+            torch.nn.Linear(H, O),
         )
     a = Approximator(net)
 
-    x = [1,2,3]
+    # len: I
+    x = range(I)
     y = a.forward(x)
     # print(y)
+
+    G = 1.
+    # len: I
+    state = [0., 1., 2.]
+    state_ = None
+    action = 1
+    action_ = None
+    Gsasa = (G, state, action, state_, action_)
+    samples = [
+        Gsasa,
+        Gsasa,
+    ]
+    loss = a.batch_train(samples, gamma=0.9, semi_gradient=False)
+    # print(loss)
+    # assert
